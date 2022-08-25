e244d499c3ff29c34ee0522218fb6f39c89891ea
alidoran <alidoran@gmail.com>
2022-08-26 01:56:12 +0430

    Add Material Alert Dialog

--- android/src/main/java/alidoran/android/fragment/MyFragment.kt
+++ android/src/main/java/alidoran/android/fragment/MyFragment.kt
@@ -5,32 +5,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MyFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-    private lateinit var binding: FragmentMyBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentMyBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-//        DataBindingUtil.inflate(inflater, R.layout.fragment_my, container, false)
-        // Inflate the layout for this fragment
-        binding = FragmentMyBinding.inflate(layoutInflater)
-//        return inflater.inflate(R.layout.fragment_my, container, false)
+    ): View {
+        binding = FragmentMyBinding.inflate(layoutInflater, container , false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnAlertDialogBuilder.setOnClickListener {
+            materialAlertDialogBuilder()
+        }
+    }
+
+    private fun materialAlertDialogBuilder() =
+        MaterialAlertDialogBuilder(context!!)
+            .setTitle("My title")
+            .setMessage("My Message")
+            .setNegativeButton("Negative") { _, _ ->
+                Toast.makeText(this.context , "Negative has been chosen", Toast.LENGTH_LONG).show()
+            }
+            .setPositiveButton("Posetive") { _, _ ->
+                Toast.makeText(this.context, "Positive has been chosen", Toast.LENGTH_LONG).show()
+            }
+            .setNeutralButton("Neutral"){_, _ ->
+                Toast.makeText(this.context, "Neutral has been chosen", Toast.LENGTH_LONG).show()
+            }
+            .show()
 }
