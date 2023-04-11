ae264bb2a18f4542be0de9d16e4edce29a2f9016
alidoran <alidoran@gmail.com>
2023-04-10 19:37:36 -0700

    Coroutine Context

---
+++ thirdpartylibrarydependencies/src/main/java/alidoran/third_party/multithreading/coroutines/CoroutinesContextHelper.kt
@@ -0,0 +1,40 @@
+package alidoran.third_party.multithreading.coroutines
+
+import kotlinx.coroutines.*
+
+object CoroutinesContextHelper {
+    @OptIn(DelicateCoroutinesApi::class)
+    fun CoroutineScope.readContextName(jobs: ArrayList<Job>) {
+        jobs += launch {
+            println("Without dispatcher: ${Thread.currentThread().name}")
+        }
+        jobs += launch(Dispatchers.Default) {
+            println("default: ${Thread.currentThread().name}")
+        }
+        jobs += launch(Dispatchers.Unconfined) {
+            println("Unconfined: ${Thread.currentThread().name}")
+        }
+        jobs += launch(Dispatchers.IO) {
+            println("IO: ${Thread.currentThread().name}")
+        }
+        jobs += launch(newSingleThreadContext("mThread")) {
+            println("mThread: ${Thread.currentThread().name}")
+        }
+    }
+
+    fun unconfined() = runBlocking {
+        launch(Dispatchers.Unconfined) {
+            println("BeforeDelay: ${Thread.currentThread().name}")
+            delay(1000)
+            println("AfterDelay: ${Thread.currentThread().name}")
+        }
+    }
+
+    fun accessJobInItself() = runBlocking {
+        val job = launch {
+            println("isActive = ${coroutineContext[Job]!!.isActive}")
+        }
+        job.join()
+    }
+}
+
