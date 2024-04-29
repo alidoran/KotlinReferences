815eea0847f1f89edd45f05e71370948763b9f7b
alidoran <alidoran@gmail.com>
2024-04-29 14:24:45 +0330

    added Webview 04

--- android/src/main/java/alidoran/android/webview/WebviewActivity.kt
+++ android/src/main/java/alidoran/android/webview/WebviewActivity.kt
@@ -1,13 +1,16 @@
 package alidoran.android.webview
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import alidoran.android.R
 import alidoran.android.databinding.ActivityWebViewBinding
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.os.Bundle
 import android.util.Base64
+import android.util.Log
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
 class WebviewActivity : AppCompatActivity() {
     lateinit var binding: ActivityWebViewBinding
@@ -20,7 +23,7 @@ class WebviewActivity : AppCompatActivity() {
         // 1- android:usesCleartextTraffic="true"
         // 2- ERR_CLEARTEXT_NOT_PERMITTED
 
-        //give internet access by the following line
+        // give internet access by the following line
         // <uses-permission android:name="android.permission.INTERNET" />
 
         binding.btnShowOnWebview.setOnClickListener {
@@ -28,36 +31,63 @@ class WebviewActivity : AppCompatActivity() {
             myWebView.loadUrl("http://www.goole.com")
         }
 
-        binding.btnShowOnActivity.setOnClickListener { webviewInActivity() }
+        binding.btnShowOnActivity.setOnClickListener {
+            val myWebView = WebView(this)
+            setContentView(myWebView)
+            myWebView.loadUrl("http://www.goole.com")
+        }
 
         binding.btnLoadFromHtmlString.setOnClickListener {
             val myWebView: WebView = findViewById(R.id.webview)
             val unencodedHtml =
-                "<!DOCTYPE html>\n" +
-                        "<html>\n" +
-                        "<head>\n" +
-                        "    <title>Hello World Example</title>\n" +
-                        "</head>\n" +
-                        "<body>\n" +
-                        "    <h1>Hello World!</h1>\n" +
-                        "</body>\n" +
-                        "</html>"
+                    "<!DOCTYPE html>\n" +
+                            "<html>\n" +
+                            "<head>\n" +
+                            "    <title>Hello World Example</title>\n" +
+                            "</head>\n" +
+                            "<body>\n" +
+                            "    <h1>Hello World!</h1>\n" +
+                            "</body>\n" +
+                            "</html>"
             val encodedHtml = Base64.encodeToString(unencodedHtml.toByteArray(), Base64.NO_PADDING)
             myWebView.loadData(encodedHtml, "text/html", "base64")
         }
+
+        binding.btnGetJavascriptResult.setOnClickListener { webviewInActivity() }
     }
 
-//    @SuppressLint("SetJavaScriptInterface")
+    @SuppressLint("SetJavaScriptEnabled")
     private fun webviewInActivity() {
-        val myWebView = WebView(this)
-        setContentView(myWebView)
-        myWebView.loadUrl("http://www.goole.com")
+        val myWebView: WebView = findViewById(R.id.webview)
+        val webSettings = myWebView.settings
+        webSettings.javaScriptEnabled = true
+        myWebView.webViewClient = webViewClient
+        myWebView.addJavascriptInterface(JSInterface(), "JSInterface")
+
+        myWebView.loadUrl("file:///android_res/raw/index.html")
     }
 
-    @JavascriptInterface
-    fun observeWebResult(){
+    private val webViewClient = object : WebViewClient() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            Log.d("webviewInActivity", "onPageFinished: $url")
+        }
 
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            Log.d("webviewInActivity", "onPageStarted: $url")
+        }
     }
 
+    class JSInterface internal constructor() {
+        @JavascriptInterface
+        fun toastMe(text: String?) {
+            Log.d("WebviewActivity", "toastMe: $text")
+        }
 
+        @JavascriptInterface
+        fun notifyMe(text: String?) {
+            Log.d("WebviewActivity", "toastMe: $text")
+        }
+    }
 }
