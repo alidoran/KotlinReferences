6a7f521f5309cc7f162d853338847adf9a1a789a
alidoran <alidoran@gmail.com>
2024-04-29 09:18:41 +0330

    - added Webview 02

--- android/src/main/java/alidoran/android/webview/webViewActivity.kt
+++ android/src/main/java/alidoran/android/webview/webViewActivity.kt
@@ -1,21 +1,55 @@
 package alidoran.android.webview
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import alidoran.android.R
+import alidoran.android.databinding.ActivityWebViewBinding
+import android.util.Base64
 import android.webkit.WebView
 
 class webViewActivity : AppCompatActivity() {
+    lateinit var binding : ActivityWebViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_web_view)
+        binding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // add the first line code in the manifest application to solve the second line error
         // 1- android:usesCleartextTraffic="true"
         // 2- ERR_CLEARTEXT_NOT_PERMITTED
-        val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.loadUrl("http://www.goole.com")
+
+        //give internet access by the following line
+        // <uses-permission android:name="android.permission.INTERNET" />
+
+        binding.btnShowOnWebview.setOnClickListener{
+            val myWebView: WebView = findViewById(R.id.webview)
+            myWebView.loadUrl("http://www.goole.com")
+        }
+
+        binding.btnShowOnActivity.setOnClickListener {
+            val myWebView = WebView(this)
+            setContentView(myWebView)
+            myWebView.loadUrl("http://www.goole.com")
+        }
+
+        binding.btnLoadFromHtmlString.setOnClickListener{
+            val myWebView: WebView = findViewById(R.id.webview)
+            val unencodedHtml =
+                "<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "<head>\n" +
+                        "    <title>Hello World Example</title>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "    <h1>Hello World!</h1>\n" +
+                        "</body>\n" +
+                        "</html>"
+            val encodedHtml = Base64.encodeToString(unencodedHtml.toByteArray(), Base64.NO_PADDING)
+            myWebView.loadData(encodedHtml, "text/html", "base64")
+        }
+
+
+
+
     }
 }
